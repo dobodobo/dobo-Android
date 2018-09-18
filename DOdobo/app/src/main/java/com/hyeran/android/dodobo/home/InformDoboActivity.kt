1cb2b0bbd752ae2e443b9c3e39bf66d1af142ea1
HyeranShin <hyebaragi__@naver.com>
2018-09-18 21:00:53 +0900

    도보관광, Seoulight 소개 페이지 완성

--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformDoboActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformDoboActivity.kt
@@ -3,11 +3,14 @@ package com.hyeran.android.dodobo.home
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeran.android.dodobo.R
+import kotlinx.android.synthetic.main.activity_inform_dobo.*
 
 class InformDoboActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inform_dobo)
+        btn_back_informdobo.setOnClickListener {
+            finish()
+        }
     }
 }
--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformSeoulightActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformSeoulightActivity.kt
@@ -3,11 +3,15 @@ package com.hyeran.android.dodobo.home
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeran.android.dodobo.R
+import kotlinx.android.synthetic.main.activity_inform_seoulight.*
 
 class InformSeoulightActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inform_seoulight)
+        btn_back_informseoulight.setOnClickListener {
+            finish()
+        }
     }
 }
