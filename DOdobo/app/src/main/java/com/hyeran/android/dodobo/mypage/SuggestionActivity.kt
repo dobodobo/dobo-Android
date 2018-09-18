20191251378f760e980e28b8816fabc361613137
HyeranShin <hyebaragi__@naver.com>
2018-09-19 00:29:43 +0900

    건의사항 뷰 완성

--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/SuggestionActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/SuggestionActivity.kt
@@ -3,11 +3,15 @@ package com.hyeran.android.dodobo.mypage
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeran.android.dodobo.R
+import kotlinx.android.synthetic.main.activity_suggestion.*
 
 class SuggestionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suggestion)
+        btn_back_suggestion.setOnClickListener {
+            finish()
+        }
     }
 }
