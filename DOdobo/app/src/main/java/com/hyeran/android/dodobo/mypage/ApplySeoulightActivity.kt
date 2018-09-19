b4fc2749c45e1a5a9cf3a747c5494d67bf222c75
HyeranShin <hyebaragi__@naver.com>
2018-09-19 10:11:10 +0900

    시민해설사 신청하기 뷰 완성

--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/ApplySeoulightActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/ApplySeoulightActivity.kt
@@ -3,11 +3,15 @@ package com.hyeran.android.dodobo.mypage
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeran.android.dodobo.R
+import kotlinx.android.synthetic.main.activity_apply_seoulight.*
 
 class ApplySeoulightActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apply_seoulight)
+        btn_back_applyseoulight.setOnClickListener {
+            finish()
+        }
     }
 }
