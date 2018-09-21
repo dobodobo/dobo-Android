3d48ad4b6da2e13ce076e99959c9a1957409d02d
HyeranShin <hyebaragi__@naver.com>
2018-09-21 23:28:52 +0900

    <프로필 수정> 기능 추가, 뷰 수정

--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/ModifyActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/mypage/ModifyActivity.kt
@@ -3,17 +3,23 @@ package com.hyeran.android.dodobo.mypage
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType.*
+import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import com.hyeran.android.dodobo.R
 import kotlinx.android.synthetic.main.activity_modify.*
-import android.text.InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-import android.text.InputType.TYPE_CLASS_TEXT
-
+import android.widget.RelativeLayout
 
 
 class ModifyActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    // 비밀번호 보이기 아이콘 Pressed 상태(0-Unpressed, 1-Pressed)
+    var status1 = 0
+    var status2 = 0
+    // 초기 inputType 저장할 변수
+    var inputTypeInvisible = 0
+//    val dialog = CompleteDialog(this)
 
     override fun onClick(v: View?) {
         when(v) {
@@ -21,14 +27,39 @@ class ModifyActivity : AppCompatActivity(), View.OnClickListener {
                 finish()
             }
             btn_show1_modify -> {
-                et_password1_modify.inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                if(status1 == 0) {  // 비밀번호 보이게
+                    status1 = 1
+                    et_password1_modify.inputType = TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                    et_password1_modify.setSelection(et_password1_modify.text.length)   // 커서 위치 이동
+                } else {            // 비밀번호 안보이게
+                    status1 = 0
+                    et_password1_modify.inputType = inputTypeInvisible
+                    et_password1_modify.setSelection(et_password1_modify.text.length)   // 커서 위치 이동
+                }
             }
             btn_show2_modify -> {
-                et_password2_modify.inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                if(status2 == 0) {  // 비밀번호 보이게
+                    status2 = 1
+                    et_password2_modify.inputType = TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                    et_password2_modify.setSelection(et_password2_modify.text.length)   // 커서 위치 이동
+                } else {            // 비밀번호 안보이게
+                    status2 = 0
+                    et_password2_modify.inputType = inputTypeInvisible
+                    et_password2_modify.setSelection(et_password2_modify.text.length)   // 커서 위치 이동
+                }
             }
             btn_applyseoulight_modify -> {
                 val intent = Intent(applicationContext, ApplySeoulightActivity::class.java)
                 startActivity(intent)
+            }
+            btn_complete_modify -> {
+                val linear = View.inflate(applicationContext, R.layout.toast_complete_modify, null) as RelativeLayout
+                val toastComplete = Toast(applicationContext)
+
+                toastComplete.view = linear
+                toastComplete.setGravity(Gravity.CENTER, 0, 0)  // 중앙으로 위치 이동(첫번째 인자를 중심으로 xOffset, yOffset 떨어진 곳)
+
+                toastComplete.show()
             }
         }
     }
@@ -36,9 +67,25 @@ class ModifyActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify)
+
+        // 클릭 리스너 등록
         btn_back_modify.setOnClickListener(this)
         btn_show1_modify.setOnClickListener(this)
         btn_show2_modify.setOnClickListener(this)
         btn_applyseoulight_modify.setOnClickListener(this)
+        btn_complete_modify.setOnClickListener(this)
+
+        // EditText 커서 위치 끝으로 이동
+        et_password1_modify.setSelection(et_password1_modify.text.length)
+        et_password2_modify.setSelection(et_password2_modify.text.length)
+
+        // INVISIBLE PASSWORD inputType 저장
+        inputTypeInvisible = et_password1_modify.inputType
+
+        //TODO btn_applyseoulight_modify    -> 서울라이트가 아닌 경우에만 보이도록
+        //TODO icon_seoulight_modify        -> 서울라이트인 경우에만 보이도록
+        //TODO btn_changephoto_modify       -> 카메라 기능 추가
+        //TODO btn_complete_modify          -> 비밀번호 불일치 토스트창
+        //TODO btn_complete_modify          -> 토스트창 띄울 때 뒤에 배경 어둡게
     }
 }
