901165ff17a0c43fc59dc23fced8903e852779de
HyeranShin <hyebaragi__@naver.com>
2018-09-21 21:11:20 +0900

    도보관광, 서울라이트 소개페이지 최종

--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformDoboActivity.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/home/InformDoboActivity.kt
@@ -1,5 +1,7 @@
 package com.hyeran.android.dodobo.home
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hyeran.android.dodobo.R
@@ -11,6 +13,11 @@ class InformDoboActivity : AppCompatActivity() {
         setContentView(R.layout.activity_inform_dobo)
         btn_back_informdobo.setOnClickListener {
             finish()
+        }
+        // 도보관광 자세히보기 홈페이지 링크
+        btn_link_informdobo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://korean.visitseoul.net/walkingtour/%EB%8F%84%EB%B3%B4%EA%B4%80%EA%B4%91%EC%9D%B4%EB%9E%80_/15020"))
+            startActivity(intent)
         }
     }
 }
