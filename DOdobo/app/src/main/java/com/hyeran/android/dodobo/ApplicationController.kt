0829e87878cf3228cca8fccf8488c04a770cd0ef
HyeranShin <hyebaragi__@naver.com>
2018-09-23 17:06:08 +0900

    통신 환경 세팅

---
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/ApplicationController.kt
@@ -0,0 +1,29 @@
+package com.hyeran.android.dodobo
+
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApplicationController : Application(){
+    lateinit var networkService: NetworkService
+    private val baseUrl = "http://52.78.25.56:3000/"
+
+    companion object {
+        lateinit var instance : ApplicationController
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this // 어느 액티비티에서든지 '나'를 할당
+        buildNetwork()
+    }
+
+    fun buildNetwork() {
+        val builder = Retrofit.Builder()
+        var retrofit = builder
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create()) // GSON을 JSON으로 쓰기
+                .build()    // baseUrl을 가지고 통신
+        networkService = retrofit.create(NetworkService::class.java)
+    }
+}
---
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/NetworkService.kt
@@ -0,0 +1,4 @@
+package com.hyeran.android.dodobo
+
+interface NetworkService {
+}
