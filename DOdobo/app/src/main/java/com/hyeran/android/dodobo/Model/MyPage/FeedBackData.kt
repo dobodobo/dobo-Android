84e71b8b6d21cbe7670055bdd9faab70f243a8a2
kyeahen <bright19961@naver.com>
2018-09-23 23:29:34 +0900

    건의사항 통신

---
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/Model/MyPage/FeedBackData.kt
@@ -0,0 +1,6 @@
+package com.hyeran.android.dodobo.Model.MyPage
+
+data class FeedBackData (
+        var title: String,
+        var content: String
+)
--- DOdobo/app/src/main/java/com/hyeran/android/dodobo/Model/SignIn/SignInResponse.kt
+++ DOdobo/app/src/main/java/com/hyeran/android/dodobo/Model/SignIn/SignInResponse.kt
@@ -7,19 +7,11 @@ import com.hyeran.android.dodobo.Model.BaseModel
 
 data class SignInResponse(
 
-        @SerializedName("status")
-        @Expose
-        open var status : Int?=null,
-
-        @SerializedName("message")
-        @Expose
-        open var message : String?=null,
-
         @SerializedName("result")
         @Expose
         open var result : SignInResult?=null
 
-)
+) : BaseModel()
 
 
 data class SignInResult(
